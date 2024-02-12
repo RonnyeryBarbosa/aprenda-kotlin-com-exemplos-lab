f6386d36fb9651c93756e11f90120d4b823ff785
Ronnyery Barbosa <ronnyerybarbosa@gmail.com>
2024-02-12 11:22:48 -0300

    Solução Proposta

--- desafio.kt
+++ desafio.kt
@@ -66,8 +66,5 @@ fun main() {
     println()
 	formacao2.listMatriculados()
    
-    
-    
-   //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
 }
